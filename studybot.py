f81804fb1e1175015d0a3d9ab8e9d06ef95a527f
sofia <sofia@iassessments.com>
2022-04-11 14:52:55 +0200

    u

--- studybot.py
+++ studybot.py
@@ -134,7 +134,9 @@ def reply(update: Update, context: CallbackContext):
                 update.message.reply_text(task_text)
                 audio_url = 'http://159.223.234.185:5000/' + title
                 print(audio_url)
-                context.bot.send_audio(chat_id=update.effective_chat.id, audio='http://159.223.234.185:5000/hello.mp3?random=64', title='Play me!')
+                context.bot.send_audio(chat_id=update.effective_chat.id,
+                                       audio='https://www.book2.nl/book2/EN/SOUND/0104.mp3')
+                context.bot.send_audio(chat_id=update.effective_chat.id, audio='http://159.223.234.185:5000/hello.mp3')
                 update.message.reply_text(task_text)
             else:
                 update.message.reply_text(task_line)
