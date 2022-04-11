8af00d93dbb8c542fec9a3acb8e68b5303ca0129
traugutt <sofsofsymko@gmail.com>
2022-04-11 12:13:37 +0000

    u

---
+++ fileserver.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+from flask import Flask, render_template, send_file
+
+
+app = Flask(__name__, static_url_path="", static_folder="bot_audio")
+
+
+@app.route("/<path:filename>")
+def serve(filename):
+    return send_static_file(filename)
+
+
