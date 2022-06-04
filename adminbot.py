a7ddb5d38da2d9b09256621b9990f7b8e3784416
sofia <sofia@iassessments.com>
2022-06-04 21:56:42 +0200

    upd

--- adminbot.py
+++ adminbot.py
@@ -110,15 +110,13 @@ def reply(update: Update, context: CallbackContext):
             questions.update_one({'_id': element_id},
                                  {"$set": {"assigned_to": [], "completed_by": [], "case_sensitive": False}})
 
-    pattern_matcher = re.findall('^topic task', command)
+    pattern_matcher = re.findall('^topic,task,original', command)
     if len(pattern_matcher) >= 1:
-        pattern = pattern_matcher[0]
-        tsv_to_csv = re.sub("\t", ",", pattern)
         with open("new_task_set.csv", 'w') as csv_file:
-            csv_file.write(tsv_to_csv)
-        os.system('mongoimport --host=127.0.0.1 -d students -c bot_data --type csv --file new_task_set.csv --headerline')
+            csv_file.write(command)
+        import_results = os.system('mongoimport --host=127.0.0.1 -d students -c bot_data --type csv --file new_task_set.csv --headerline')
 
-        update.message.reply_text('db updated')
+        update.message.reply_text(import_results)
 
     pattern_matcher = re.findall('assign [A-z_0-9]+ to [A-z_0-9]+', command)
     if len(pattern_matcher) >= 1:
