49fd1209f465013bff98dd8887ac1c8678223a5b
traugutt <sofsofsymko@gmail.com>
2022-11-19 20:39:40 +0200

    changesss

--- adminbot.py
+++ adminbot.py
@@ -1,31 +1,13 @@
-# -*- coding: utf-8 -*-
-#!/usr/bin/env python
-# pylint: disable=C0116,W0613
-# This program is dedicated to the public domain under the CC0 license.
-
-"""
-Basic example for a bot that uses inline keyboards. For an in-depth explanation, check out
- https://git.io/JOmFw.
-"""
+import datetime
 import logging
-import time
 import re
-import io
 import os
-import sys
-# from daemonize import Daemonize
-
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, KeyboardButton, ReplyKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext
+from telegram import Update
+from telegram.ext import CallbackContext
 from telegram.ext import Updater, CommandHandler
 from telegram.ext import MessageHandler, Filters
-import datetime
-
-# from homework_bot.__init__ import app
-# from mindmeld.components.dialogue import Conversation
-
+from langdetect import detect
 from pymongo import MongoClient
-# pid = "/tmp/studybot.pid"
 
 client = MongoClient('mongodb://localhost:27017/')
 db = client.students
@@ -38,128 +20,106 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-# def start(update, context):
-#     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+user = None
 
-#conversations = {}
+client = MongoClient('mongodb://localhost:27017/')
+db = client.students
+questions = db['bot_data']
+
 
 def assign_n_tasks(topic, username):
-    res = questions.find({'topic':topic, "assigned_to":{"$nin":[username]},"completed_by":{"$nin":[username]}})
-    # res = collection.findMany({"assigned_to":{"$nin":[username]},"completed_by":{"$nin":[username]}})
+    res = questions.find({'topic': topic, "assigned_to": {"$nin": [username]}, "completed_by": {"$nin": [username]}})
     counter = 0
     for i in res:
         element_id = i['_id']
         task = i['original']
-        questions.update_one({'_id':element_id},{'$push':{"assigned_to":username}})
-        counter+=1
+        questions.update_one({'_id': element_id}, {'$push': {"assigned_to": username}})
+        counter += 1
     return 'Assigned %s from %s to %s' % (counter, topic, username)
 
 
 def user_stats(username):
     topics = {}
     assigned_not_completed_count = 0
-    assigned_not_completed = questions.find({"assigned_to":{"$in":[username]},"completed_by":{"$nin":[username]}})
+    assigned_not_completed = questions.find({"assigned_to": {"$in": [username]}, "completed_by": {"$nin": [username]}})
 
     for i in assigned_not_completed:
-        assigned_not_completed_count +=1
-        topic = i['topic']
-        try:
-            topics[topic]+=1
-        except KeyError:
-            topics[topic]=0
+        assigned_not_completed_count += 1
 
-    print(topics)
-
-    result = '%s has ' % (username) + str(assigned_not_completed_count) + ' tasks left to complete\nThe topic are:\n'
+    result = '%s has ' % (username) + str(assigned_not_completed_count) + ' tasks left to complete'
     for i in topics.keys():
         topic_stat = i + ' ' + str(topics[i]) + '\n'
-        result+=topic_stat
+        result += topic_stat
     return result
 
+
 def remove_tasks(topic, username):
-    res = questions.find({'topic':topic, "assigned_to":{"$in":[username]},"completed_by":{"$nin":[username]}})
+    res = questions.find({'topic': topic, "assigned_to": {"$in": [username]}, "completed_by": {"$nin": [username]}})
     for i in res:
         element_id = i['_id']
         task = i['original']
-        questions.update_one({'_id':element_id},{'$pull':{"assigned_to":username}})
+        questions.update_one({'_id': element_id}, {'$pull': {"assigned_to": username}})
     return 'Removed %s from %s' % (topic, username)
 
 
-
 def start(update: Update, context: CallbackContext):
-    
-    # total = questions.count_documents({"assigned_to":{"$in":[username]}, "completed_by":{"$nin":[username]}})
-
     update.message.reply_text("Hello teacher!")
 
 
 def reply(update: Update, context: CallbackContext):
     command = update.message.text
-    print(command)
-
-    pattern_matcher = re.findall('db remove [a-z0-9_]+', command)
-    if len(pattern_matcher) >= 1:
-        topic = pattern_matcher[0].split(' ')
-        topic = topic[2]
-        print(topic)
-        client = MongoClient('mongodb://localhost:27017/')
-        db = client.students
-        questions = db['bot_data']
-
-        questions.delete_many({'topic': topic})
-        number_of_questions = questions.find({'topic': topic})
-        if len(list(number_of_questions)) == 0:
-            update.message.reply_text(f'removed {topic} from db')
-
-    pattern_matcher = re.findall('^topic,task,original', command)
-    if len(pattern_matcher) >= 1:
-        with open("new_task_set.csv", 'w') as csv_file:
-            csv_file.write(command)
-        os.system('mongoimport --host=127.0.0.1 -d students -c bot_data --type csv --file new_task_set.csv --headerline')
-        update.message.reply_text('db updated')
-
-    pattern_matcher = re.findall('[A-z_0-9]+ to [A-z_0-9]+', command)
-    if len(pattern_matcher) >= 1:
-        pattern = pattern_matcher[0]
-        pattern = pattern.split(' ')
-        topic = pattern[0]
-        username = pattern[2]
-        res = assign_n_tasks(topic, username)
-        update.message.reply_text(res)
-
-    # pattern_matcher = re.findall('[A-z_0-9]+ to [A-z_0-9]+', command)
-    # if len(pattern_matcher) >= 1:
-    #     pattern = pattern_matcher[0]
-    #     pattern = pattern.split(' ')
-    #     topic = pattern[0]
-    #     username = pattern[2]
-    #     res = assign_n_tasks(topic, username)
-    #     update.message.reply_text(res)
-
     pattern_matcher = re.findall('[Ss]tats [A-z_]+', command)
     if len(pattern_matcher) >= 1:
         pattern = pattern_matcher[0]
         pattern = pattern.split(' ')
         username = pattern[1]
         res = user_stats(username)
-        update.message.reply_text(res)
+        return update.message.reply_text(res)
+    if context.chat_data.get('step', None) == 0 or not context.chat_data.get('step', None):
+        context.chat_data['username'] = command
+        context.chat_data['step'] = 1
+        return update.message.reply_text("Please insert term")
+    if context.chat_data.get('step', None) == 1:
+        context.chat_data['text'] = command
+        context.chat_data['step'] = 2
+        return update.message.reply_text("Please insert translation")
+    if context.chat_data.get('step', None) == 2:
+        context.chat_data['translation'] = command
+        lang = detect(context.chat_data.get('text', "English"))
+        if lang not in ['en', 'ko']:
+            lang = "en"
+        query = {
+            "topic": "",
+            "task": context.chat_data['translation'],
+            "original": context.chat_data['text'],
+            "modified_original": context.chat_data['translation'],
+            "max_attempts": 3,
+            "audio": "yes",
+            "lang": lang,
+            "assigned_to": [
+                context.chat_data['username']
+            ],
+            "completed_by": [
+            ],
+            "created": str(datetime.date.today())
+        }
+        questions.insert_one(query)
+        update.message.reply_text(f"Inserted {str(query)}")
+        context.chat_data['step'] = 0
+        return
 
+def add(update: Update, context: CallbackContext):
+    update.message.reply_text("Please add username")
 
-    pattern_matcher = re.findall('[Rr]emove [A-z_0-9]+ from [A-z_0-9]+', command)
-    if len(pattern_matcher) >= 1:
-        pattern = pattern_matcher[0]
-        pattern = pattern.split(' ')
-        topic = pattern[1]
-        username = pattern[3]
-        res = remove_tasks(topic, username)
-        update.message.reply_text(res)
 
 def audio(update: Update, context: CallbackContext):
     context.bot.send_audio(chat_id=update.effective_chat.id, audio=open('./001-poklon-razogrev.mp3', 'rb'))
 
+
 def help_command(update: Update, context):
     """Displays info on how to use the bot."""
     update.message.reply_text("Use /start to test this bot.")
+
 
 def stop(update: Update, context):
     update.message.reply_text("Use /start to start again.")
@@ -175,20 +135,11 @@ def main() -> None:
     updater.dispatcher.add_handler(CommandHandler('help', help_command))
     updater.dispatcher.add_handler(CommandHandler('stop', stop))
     updater.dispatcher.add_handler(CommandHandler('audio', audio))
+    updater.dispatcher.add_handler(CommandHandler('add', add))
     updater.dispatcher.add_handler(MessageHandler(Filters.text, reply))
-
-
-    # Start the Bot
     updater.start_polling()
-
-    # Run the bot until the user presses Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT
     updater.idle()
 
 
 if __name__ == '__main__':
     main()
-
-# daemon = Daemonize(app="studybot", pid=pid, action=main)
-# daemon.start()
-
--- studybot.py
+++ studybot.py
@@ -8,23 +8,16 @@ Basic example for a bot that uses inline keyboards. For an in-depth explanation,
  https://git.io/JOmFw.
 """
 import logging
-import time
-# from daemonize import Daemonize
 
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, KeyboardButton, ReplyKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext
 from telegram.ext import Updater, CommandHandler
 from telegram.ext import MessageHandler, Filters
 import datetime
-import os
 from navertts import NaverTTS
 import uuid
-import os
-# from homework_bot.__init__ import app
-# from mindmeld.components.dialogue import Conversation
-
 from pymongo import MongoClient
-# pid = "/tmp/studybot.pid"
+
 
 client = MongoClient('mongodb://localhost:27017/')
 db = client.students
@@ -40,17 +33,17 @@ logger = logging.getLogger(__name__)
 # def start(update, context):
 #     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
-#conversations = {}
-
 
 def start(update: Update, context: CallbackContext):
     username = update.message.chat.username
-    total = questions.count_documents({"assigned_to":{"$in":[username]}, "completed_by":{"$nin":[username]}})
+    total = questions.count_documents({"assigned_to": {"$in": [username]}, "completed_by": {"$nin": [username]}})
 
-    if total == 0:
-        message_reply_text = 'There\'s no homework for you to do at the moment. Press /start to check for homework at a later time.'
+    if not total:
+        message_reply_text = 'There\'s no homework for you to do at the moment. ' \
+                             'Press /start to check for homework at a later time.'
     else:
-        message_reply_text = 'Welcome, you have ' + str(total) + ' questions left to answer. Ready to do your homework? Type in "Y or y" to proceed.'
+        message_reply_text = f'Welcome, you have {str(total)} questions left to answer. ' \
+                             f'Ready to do your homework? Type in "Y or y" to proceed.'
     
     # keyboard = [
     # [
@@ -65,23 +58,28 @@ def start(update: Update, context: CallbackContext):
     # update.message.reply_text(message_reply_text, reply_markup=reply_markup)
     update.message.reply_text(message_reply_text)
 
-def check_answer(correct_answer, provided_answer):
-            if type(correct_answer) == list:
-                if provided_answer in correct_answer:
-                    return True
-            elif type(correct_answer) == str:
-                print(f'correct answer: {correct_answer}')
-                print(f'provided answer: {provided_answer}')
-                if provided_answer == correct_answer:
-                    return True
 
-def create_tts(text, lang):
+def check_answer(correct_answer, provided_answer):
+    if type(correct_answer) == list:
+        if provided_answer in correct_answer:
+            return True
+    elif type(correct_answer) == str:
+        print(f'correct answer: {correct_answer}')
+        print(f'provided answer: {provided_answer}')
+    if provided_answer == correct_answer:
+        return True
+    else:
+        return False
+
+
+def create_tts(text, lang, username):
     tts = NaverTTS(text, lang=lang)
     text = uuid.uuid4()
     mp3_title = str(text) + '.mp3'
     tts.save('bot_audio/' + mp3_title)
     print('audio saved')
     return mp3_title
+
 
 def reply(update: Update, context: CallbackContext):
 
@@ -94,61 +92,60 @@ def reply(update: Update, context: CallbackContext):
         for char in split_chars:
             if char == '’':
                 char = '\''
-            res+=char
+            res += char
         previous_answer = res
     print(f'previous_answer: {previous_answer}')
 
     if previous_answer.lower() == 'y':
         print(previous_answer)
 
-        question = questions.find_one({"assigned_to":{"$in":[username]}, "completed_by":{"$nin":[username]}})
+        question = questions.find_one({"assigned_to": {"$in": [username]},
+                                       "completed_by": {"$nin": [username]}})
 
-        try:
-            task_line = question['task']
-        except TypeError:
-            update.message.reply_text('There\'s no homework for you to do at the moment. Press /start to check for homework at a later time.')
+        if not bool(question):
+            update.message.reply_text('There\'s no homework for you to do at the moment. '
+                                      'Press /start to check for homework at a later time.')
             return
 
         task_text = question['modified_original']
-        print(task_text)
         correct_answer = question['original']
         element_id = question['_id']
         task_line = question['task']
-        topic = question['topic']
-            
-        #questions.update_one({'_id':element_id},{'$set': {"completed_by": username }})
-        
-        is_answered = answers.find_one({'question_id':element_id, 'username':username})
 
-        try:
-            is_answered['number_of_tries']
-            answers.update_one({'question_id':element_id,'username':username},{'$set':{'time':datetime.datetime.utcnow()}})
-        except TypeError:
-            answers.insert_one({'question_id':element_id,'username':username,'correct_answer':correct_answer, 'time':datetime.datetime.utcnow(), 'number_of_tries':0, 'number_of_tries_historic':0, 'number_of_times_answered':0})
-        try:
-            audio = question['audio']
-            if audio == 'yes':
-                lang = question['lang']
-                title = create_tts(correct_answer, lang)
-                path_to_file = 'bot_audio/' + title
-                update.message.reply_text(task_line)
-                context.bot.send_audio(chat_id=update.effective_chat.id, audio=open(path_to_file, 'rb'))
-            elif 'http' in audio:
-                update.message.reply_text(task_line)
-                context.bot.send_audio(chat_id=update.effective_chat.id, audio=audio)
-            else:
-                update.message.reply_text(task_line)
-                update.message.reply_text(task_text)
-        except KeyError:
-            update.message.reply_text(task_text)
+        is_answered = answers.find_one({'question_id': element_id, 'username': username})
+
+        if bool(is_answered):
+            answers.update_one({'question_id': element_id, 'username': username},
+                               {'$set': {'time': datetime.datetime.utcnow()}})
+        else:
+            answers.insert_one({'question_id': element_id,
+                                'username': username,
+                                'correct_answer': correct_answer,
+                                'time': datetime.datetime.utcnow(),
+                                'number_of_tries': 0,
+                                'number_of_tries_historic': 0,
+                                'number_of_times_answered': 0})
+
+        audio = question.get('audio', None)
+        if audio:
+            lang = question['lang']
+            title = create_tts(correct_answer, lang, username)
+            path_to_file = 'bot_audio/' + title
             update.message.reply_text(task_line)
+            context.bot.send_audio(chat_id=update.effective_chat.id, audio=open(path_to_file, 'rb'))
+        elif 'http' in audio:
+            update.message.reply_text(task_line)
+            context.bot.send_audio(chat_id=update.effective_chat.id, audio=audio)
+        else:
+            update.message.reply_text(task_line)
+            update.message.reply_text(task_text)
 
     else:
         previous_questions_raw = answers.find({'username': username}).sort([('time', -1)])
         previous_questions_sorted = []
 
-        for i in previous_questions_raw:
-            previous_questions_sorted.append(i)
+        for question in previous_questions_raw:
+            previous_questions_sorted.append(question)
 
         previous_question = previous_questions_sorted[0]
         element_id = previous_question['_id']
@@ -158,31 +155,26 @@ def reply(update: Update, context: CallbackContext):
         element_id = previous_question['question_id']
         print(element_id)
         print(username)
-        is_case_sensitive = questions.find_one({'_id':element_id})['case_sensitive']
+        is_case_sensitive = questions.find_one({'_id': element_id}).get('case_sensitive', False)
 
-        if username in ['Maxon_Stenduper','traugutt']:
-            previous_answer = previous_answer.lower()
-            correct_previous = correct_previous.lower()
-            if previous_answer[-1] == '.':
-                previous_answer = previous_answer[:-1]
-            if correct_previous[-1] == '.':
-                correct_previous = correct_previous[:-1]
-
-        #disable case sensitive
-        if is_case_sensitive == False:
+        if not is_case_sensitive:
             previous_answer = previous_answer.lower()
             correct_previous = correct_previous.lower()
 
+        is_correct = check_answer(correct_previous, previous_answer)
 
-        is_correct = check_answer(correct_previous,previous_answer)
-
-        if is_correct == True:
-            questions.update_one({'_id':element_id},{'$push': {"completed_by": username }})
-            question = questions.find_one({"assigned_to":{"$in":[username]}, "completed_by":{"$nin":[username]}})
-            answers.update_one({'question_id':element_id, 'username':username},{'$inc': {'number_of_times_answered':1}})
-            answers.update_one({'question_id':element_id, 'username':username},{'$set': {'time':datetime.datetime.utcnow()}})
-            if question == None:
-                update.message.reply_text('Congrats! You did all your homework! Press /start to check for new homework at a later time.')
+        if is_correct:
+            questions.update_one({'_id': element_id}, {'$push': {"completed_by": username}})
+            question = questions.find_one({"assigned_to": {"$in": [username]}, "completed_by": {"$nin": [username]}})
+            answers.update_one(
+                {'question_id': element_id, 'username': username},
+                {'$inc': {'number_of_times_answered': 1}})
+            answers.update_one(
+                {'question_id': element_id, 'username': username},
+                {'$set': {'time': datetime.datetime.utcnow()}})
+            if not question:
+                update.message.reply_text('Congrats! You did all your homework! '
+                                          'Press /start to check for new homework at a later time.')
             else:
 
                 print(question)
@@ -190,35 +182,42 @@ def reply(update: Update, context: CallbackContext):
                 task_text = question['modified_original']
                 correct_answer = question['original']
                 element_id = question['_id']
-                is_answered = answers.find_one({'question_id':element_id,'username':username})
+                is_answered = answers.find_one({'question_id': element_id, 'username': username})
 
-                try:
-                    is_answered['number_of_tries']
-                    answers.update_one({'question_id':element_id,'username':username},{'$set':{'time':datetime.datetime.utcnow()}})
-                except TypeError:
-                    answers.insert_one({'question_id':element_id,'username':username,'correct_answer':correct_answer, 'time':datetime.datetime.utcnow(), 'number_of_tries':0, 'number_of_tries_historic':0, 'number_of_times_answered':0})
-                try:
-                    audio = question['audio']
-                    if audio == 'yes':
-                        lang = question['lang']
-                        title = create_tts(correct_answer, lang)
-                        path_to_file = 'bot_audio/' + title
-                        update.message.reply_text(task_line)
-                        context.bot.send_audio(chat_id=update.effective_chat.id, audio=open(path_to_file, 'rb'))
-                    else:
-                        update.message.reply_text(task_line)
-                        update.message.reply_text(task_text)
-                except KeyError:
+                if is_answered:
+                    answers.update_one({'question_id': element_id, 'username': username},
+                                       {'$set': {'time': datetime.datetime.utcnow()}})
+                else:
+                    answers.insert_one({'question_id': element_id,
+                                        'username': username,
+                                        'correct_answer': correct_answer,
+                                        'time': datetime.datetime.utcnow(),
+                                        'number_of_tries': 0,
+                                        'number_of_tries_historic': 0,
+                                        'number_of_times_answered': 0})
+
+                audio = question.get('audio', None)
+                if audio:
+                    lang = question['lang']
+                    title = create_tts(correct_answer, lang)
+                    path_to_file = 'bot_audio/' + title
+                    update.message.reply_text(task_line)
+                    context.bot.send_audio(chat_id=update.effective_chat.id, audio=open(path_to_file, 'rb'))
+                elif 'http' in audio:
+                    update.message.reply_text(task_line)
+                    context.bot.send_audio(chat_id=update.effective_chat.id, audio=audio)
+                else:
                     update.message.reply_text(task_line)
                     update.message.reply_text(task_text)
         else:
-            num_of_tries = answers.find_one({'question_id':element_id, 'username':username})['number_of_tries']
-            max_attempts = questions.find_one({'_id':element_id})['max_attempts']
+            num_of_tries = answers.find_one({'question_id': element_id, 'username': username})['number_of_tries']
+            max_attempts = 3
             if num_of_tries < max_attempts:
                 update.message.reply_text('Please try again.')
-                answers.update_one({'question_id':element_id, 'username':username},{"$inc": {'number_of_tries':1, 'number_of_tries_historic':1}})
+                answers.update_one({'question_id': element_id, 'username': username},
+                                   {"$inc": {'number_of_tries': 1, 'number_of_tries_historic': 1}})
             else:
-                correct_answer = questions.find_one({'_id':element_id})['original']
+                correct_answer = questions.find_one({'_id': element_id})['original']
 
                 answer_merger = ''
                 if type(correct_answer) == list:
@@ -227,22 +226,28 @@ def reply(update: Update, context: CallbackContext):
                 
                 print(answer_merger)
 
-                explanation = questions.find_one({'_id':element_id})
+                explanation = questions.find_one({'_id': element_id}).get('explanation', None)
+                if explanation:
+                    update.message.reply_text(f'The answer is \'{correct_answer}\' \n\n{explanation}\n\n'
+                                              f'Please type in the correct answer to proceed.')
+                else:
+                    update.message.reply_text(f'The answer is \'{correct_answer}\' \n\n'
+                                              f'Please type in the correct answer to proceed.')
 
-                try:
-                    explanation = questions.find_one({'_id':element_id})['explanation']
-                    update.message.reply_text('The answer is \'' + correct_answer + '\' \n\n' + explanation + '\n\nPlease type in the correct answer to proceed.')         
-                except KeyError:         
-                    update.message.reply_text('The answer is \'' + correct_answer + '\' \n\nPlease type in the correct answer to proceed.')
+                answers.update_one({'question_id': element_id, 'username': username}, {"$set": {'number_of_tries': 0}})
 
-                answers.update_one({'question_id':element_id, 'username':username},{"$set": {'number_of_tries':0}})
-
-
-
-
+def today(update: Update, context):
+    username = update.message.chat.username
+    query = {'assigned_to':{"$in":[username]}, "created": str(datetime.date.today())}
+    res = questions.find(query)
+    data = ''
+    for entry in res:
+        data += f"{entry['original']} - {entry['modified_original']}\n"
+    return update.message.reply_text(data)
 def help_command(update: Update, context):
     """Displays info on how to use the bot."""
     update.message.reply_text("Use /start to test this bot.")
+
 
 def stop(update: Update, context):
     update.message.reply_text("Use /start to start again.")
@@ -254,14 +259,11 @@ def main() -> None:
     updater = Updater("1896847698:AAFtp1t66yDx-z8-H2m2_d_lj2eC59Q0ay4", use_context=True)
 
     updater.dispatcher.add_handler(CommandHandler('start', start))
-    #updater.dispatcher.add_handler(CallbackQueryHandler(button))
     updater.dispatcher.add_handler(CommandHandler('help', help_command))
     updater.dispatcher.add_handler(CommandHandler('stop', stop))
+    updater.dispatcher.add_handler(CommandHandler('today', today))
     updater.dispatcher.add_error_handler(start)
     updater.dispatcher.add_handler(MessageHandler(Filters.text, reply))
-
-
-    # Start the Bot
     updater.start_polling()
 
     # Run the bot until the user presses Ctrl-C or the process receives SIGINT,
