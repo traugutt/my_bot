84da52c8c8e6f3dec2fa6ba38f1a90f2bbc069c9
traugutt <sofsofsymko@gmail.com>
2023-01-28 22:25:17 +0200

    changesss

--- studybot.py
+++ studybot.py
@@ -346,7 +346,7 @@ def main() -> None:
     updater.dispatcher.add_handler(CommandHandler('stop', stop))
     updater.dispatcher.add_handler(CommandHandler('today', today))
     updater.dispatcher.add_handler(CommandHandler('add', add))
-    updater.dispatcher.add_error_handler(start)
+    #updater.dispatcher.add_error_handler(start)
     updater.dispatcher.add_handler(MessageHandler(Filters.text, reply))
     updater.start_polling()
     updater.idle()
